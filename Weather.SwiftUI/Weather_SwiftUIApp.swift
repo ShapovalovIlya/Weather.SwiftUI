5974a95dfbe69178e63be0bbae3376554db73ee5
Ilia Shapovalov <greateman@mail.ru>
2022-09-17 20:21:37 +0300

    Initial Commit

---
+++ Weather.SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather.SwiftUI
+//
+//  Created by Илья Шаповалов on 17.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
